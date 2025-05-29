3746ddd80818533b0f47e1ddf86a4c5ba5b445b9
loorenzoandre <lorenzoandrecosta@gmail.com>
2025-05-29 12:42:37 -0300

    hub.lua
    Hub para carregar scripts LC & CG

---
+++ hub.lua
@@ -0,0 +1,7 @@
+-- hub.lua
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/loorenzoandre/script-com-LC-e-CG/main/ui.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/loorenzoandre/script-com-LC-e-CG/main/utils.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/loorenzoandre/script-com-LC-e-CG/main/main.lua"))()
+
+print("LC & CG HUB carregado com sucesso!")
